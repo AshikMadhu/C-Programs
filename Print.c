bf07162125909eaedd1f8c85c3d75c09ee445850
AshikMadhu <ashiksjc2025@gmail.com>
2026-01-18 11:45:06 +0530

    Add files via upload

---
+++ Print.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name: Ashik Madhu\n");
+    printf("Age: 100\n");
+    printf("Course: B.Tech\n");
+    printf("Department: Computer Science");
+    return 0;
+}
