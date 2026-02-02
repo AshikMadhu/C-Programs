cf6073dd0e829bd53d1de4a76cdc0d4d00589c90
AshikMadhu <ashiksjc2025@gmail.com>
2026-02-02 21:14:41 +0530

    Largest.c

--- Array/Largest.c
+++ Array/Largest.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int a[5], i, max;
-    printf("Enter 5 elements:\n");
-    for (i = 0; i < 5; i++) {
+    int n, i;
+
+    printf("Enter array size: ");
+    scanf("%d", &n);
+    int a[n];
+
+    printf("Enter elements:\n");
+    for (i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
-    max = a[0];
-    for (i = 1; i < 5; i++) {
-        if (a[i] > max)
-            max = a[i];
+
+    printf("Array elements are:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", a[i]);
     }
-    printf("Largest element = %d", max);
+
     return 0;
 }
-
