1ef505bbcf3796ad320b2efae7ac6c8f26cddf0a
AshikMadhu <ashiksjc2025@gmail.com>
2026-02-09 22:19:09 +0530

    length.c

--- Strings/length.c
+++ Strings/length.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
+
 int main() {
-    int n, i = 0;
+    int n, i = 0, length = 0;
+
     printf("Enter size of string: ");
     scanf("%d", &n);
+    getchar();
+
     char str[n];
+
     printf("Enter string: ");
-    scanf("%s", str);
-    while (str[i] != '\0') {
+    fgets(str, n, stdin);
+
+    while (str[i] != '\0' && str[i] != '\n') {
+        length++;
         i++;
     }
 
-    printf("Length of string = %d", i);
-
+    printf("Length of string = %d\n", length);
     return 0;
 }
