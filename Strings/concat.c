60fc8fe41321dfb39475f829187c146db040d308
AshikMadhu <ashiksjc2025@gmail.com>
2026-02-09 20:31:45 +0530

    concat.c

--- Strings/concat.c
+++ Strings/concat.c
@@ -3,20 +3,21 @@ int main() {
     int n, i = 0, j = 0;
     printf("Enter size of strings: ");
     scanf("%d", &n);
-    char str1[n], str2[n];
+    char a[n], b[n];
     printf("Enter first string: ");
-    scanf("%s", str1);
+    scanf("%s", a);
     printf("Enter second string: ");
-    scanf("%s", str2);
-    while (str1[i] != '\0') {
+    scanf("%s", b);
+    while (a[i] != '\0') {
         i++;
     }
-    while (str2[j] != '\0') {
-        str1[i] = str2[j];
+    while (b[j] != '\0') {
+        a[i] = b[j];
         i++;
         j++;
     }
-    str1[i] = '\0';
-    printf("Concatenated string: %s", str1);
+    a[i] = '\0';
+    printf("Concatenated string: %s", a);
     return 0;
 }
+
