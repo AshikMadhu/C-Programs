4cb5713ca773b8ecaaf52d5779c91fbde02dcaa6
AshikMadhu <ashiksjc2025@gmail.com>
2026-02-09 22:18:20 +0530

    display.c

--- Strings/display.c
+++ Strings/display.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+
 int main() {
     int n;
+
     printf("Enter size of string: ");
     scanf("%d", &n);
+    getchar();
+
     char str[n];
+
     printf("Enter string: ");
-    scanf("%s", str);
+    fgets(str, n, stdin);
+
     printf("Entered string is: %s", str);
+
     return 0;
 }
