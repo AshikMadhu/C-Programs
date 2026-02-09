469177d1e07588c349831dbaf21a0a907db75660
AshikMadhu <ashiksjc2025@gmail.com>
2026-02-09 22:20:02 +0530

    lengthfun.c

--- Strings/lengthfun.c
+++ Strings/lengthfun.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+
 int main() {
     int n;
+
     printf("Enter size of string: ");
     scanf("%d", &n);
+    getchar();
+
     char str[n];
+
     printf("Enter string: ");
-    scanf("%s", str);
-    printf("Length of string = %lu", strlen(str));
+    fgets(str, n, stdin);
+
+    printf("Length of string = %lu\n", strlen(str) - 1);
     return 0;
 }
