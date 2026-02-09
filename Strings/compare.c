7808e472fe050dd74f9294fe1bb09848379c8a3b
AshikMadhu <ashiksjc2025@gmail.com>
2026-02-09 22:13:10 +0530

    compare.c

--- Strings/compare.c
+++ Strings/compare.c
@@ -1,29 +1,34 @@
 #include <stdio.h>
+
 int main() {
     int n, i = 0, flag = 0;
+
     printf("Enter size of strings: ");
     scanf("%d", &n);
+    getchar();
+
     char a[n], b[n];
+
     printf("Enter first string: ");
-    fgets(a,sizeof(a),stdin);
+    fgets(a, n, stdin);
+
     printf("Enter second string: ");
-    fgets(b,sizeof(b),stdin);
+    fgets(b, n, stdin);
+
     while (a[i] != '\0' || b[i] != '\0') {
         if (a[i] != b[i]) {
             flag = 1;
             break;
         }
-
+        if (a[i] == '\n' && b[i] == '\n')
+            break;
         i++;
-        
     }
 
     if (flag == 0)
-        printf("Strings are equal");
+        printf("Strings are equal\n");
     else
-        printf("Strings are not equal");
+        printf("Strings are not equal\n");
 
     return 0;
 }
-
-
